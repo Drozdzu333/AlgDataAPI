0bf3e8d657a1bad6a9e82f17882b670d6452cdaa
Michał Rożdżyński <michal.rozdzynski@motorolasolutions.com>
2022-08-29 13:15:29 +0200

    Added Getaway-AlgorythmsController with all functionality

--- AlgDataAPI/AlgDataGateway.API/AlgDataGateway.API/Controllers/AlgorithmsController.cs
+++ AlgDataAPI/AlgDataGateway.API/AlgDataGateway.API/Controllers/AlgorithmsController.cs
@@ -1,50 +1,52 @@
-﻿namespace AlgDataGateway.API.Controllers
+﻿using System.Text;
+
+namespace AlgDataGateway.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AlgorithmsController : ControllerBase
     {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly Uri _connectionString;
+        public AlgorithmsController(IConfiguration configuration)
+        {
+            _connectionString = new Uri(configuration.GetSection("ConnectionStrings:Algorithms").Value);
+        }
         [HttpPost]
         [Route("BubbleSort/")]
         public async Task<ActionResult<SortResultDTO>> BubbleSort([FromBody] List<int> value)
-        {
-            throw new NotImplementedException();
-            //return Ok(await bubbleSort.SortAsync(value));
-        }
+            => await Connect(value);
         [HttpPost]
         [Route("InsertSort/")]
         public async Task<ActionResult<SortResultDTO>> InsertSort([FromBody] List<int> value)
-        {
-            throw new NotImplementedException();
-            //return Ok(await insertSort.SortAsync(value));
-        }
+            => await Connect(value);
         [HttpPost]
         [Route("MergeSort/")]
         public async Task<ActionResult<SortResultDTO>> MergeSort([FromBody] List<int> value)
-        {
-            throw new NotImplementedException();
-            //return Ok(await mergeSort.SortAsync(value));
-        }
+            => await Connect(value);
         [HttpPost]
         [Route("QuickSort/")]
         public async Task<ActionResult<SortResultDTO>> QuickSort([FromBody] List<int> value)
-        {
-            throw new NotImplementedException();
-            //return Ok(await quickSort.SortAsync(value));
-        }
+            => await Connect(value);
         [HttpPost]
         [Route("SelectSort/")]
         public async Task<ActionResult<SortResultDTO>> SelectSort([FromBody] List<int> value)
-        {
-            throw new NotImplementedException();
-            //return Ok(await selectSort.SortAsync(value));
-        }
+            => await Connect(value);
         [HttpPost]
         [Route("TreeSort/")]
         public async Task<ActionResult<SortResultDTO>> TreeSort([FromBody] List<int> value)
+            => await Connect(value);
+        private async Task<ActionResult<SortResultDTO>> Connect(List<int> value)
         {
-            throw new NotImplementedException();
-            //return Ok(await treeSort.SortAsync(value));
+            var queryString = HttpContext.Request.Path.Value.Substring(1);
+            var content = new StringContent(JsonSerializer.Serialize(value).ToString(), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(_connectionString + queryString, content);
+            if (response.IsSuccessStatusCode)
+            {
+                var responceContent = await response.Content.ReadFromJsonAsync<SortResultDTO>();
+                return Ok(responceContent);
+            }
+            return BadRequest();
         }
     }
 }
